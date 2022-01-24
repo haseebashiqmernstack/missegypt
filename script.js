fabac7e9cf45e81b73a8e727da97893da10fee2e
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2022-01-24 16:52:49 +0500

    24/01/2022 1

---
+++ script.js
@@ -0,0 +1,15 @@
+var typed=new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+const nav =document.querySelector(".nav"),
+navList=nav.querySelectorAll("li"),
+totalNavList=navList.length;
+for(let i=0;i<totalNavList;i++)
+{
+    
+}
