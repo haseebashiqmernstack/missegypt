67d5af22fd99e6748e5338c51fb8fc63dc5db1f6
Mohammad Haseeb Ashiq <learndotpk@gmail.com>
2022-02-03 23:03:52 +0500

    03/02/2022 1

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
             <ul class="nav">
               <li><a href="#home" class="active"><i class="fa fa-home"></i> Home</a></li>
               <li><a href="#about"><i class="fa fa-user"></i> About</a></li>
-              <li><a href=""><i class="fa fa-list"></i> Services</a></li>
-              <li><a href=""><i class="fa fa-briefcase"></i> Portfolio</a></li>
+              <li><a href="#services"><i class="fa fa-list"></i> Services</a></li>
+              <li><a href="#portfolio"><i class="fa fa-briefcase"></i> Portfolio</a></li>
               <li><a href=""><i class="fa fa-comments"></i> Contact</a></li>
             </ul>
         </div>
@@ -64,7 +64,7 @@
                         <div class="about-content padd-15">
                             <div class="row">
                                 <div class="about-text padd-15">
-                                    <h3>I' m Safinaz Ahmad Luckey <span>Representative African Women</span></h3>
+                                    <h3>I' m Safinaz Ahmad Mubarak <span>Representative African Women</span></h3>
                                     <p>I working in GIS field with extensive experience for over 5 years.
                                         My expertise is ArcGIS,AutoCad,Cartography, and many more...
                                     </p>
@@ -152,28 +152,16 @@
                                          <div class="timeline-item">
                                             <div class="circle-dot"></div>
                                             <h3 class="timeline-date">
-                                                <i class="fa fa-calendar"></i> 2013-2015
+                                                <i class="fa fa-calendar"></i> 2012-2016
                                             </h3>
                                             <h4 class="timeline-title">
                                                 BA arts. Geography information system
                                             </h4>
                                             <p class="timeline-text">
-                                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta laborum, quibusdam unde minus doloremque ad quod quos officiis nam nesciunt.
+                                                Damanhour University
                                             </p>
                                          </div>
-                                         <!-- timeline item -->
-                                         <div class="timeline-item">
-                                            <div class="circle-dot"></div>
-                                            <h3 class="timeline-date">
-                                                <i class="fa fa-calendar"></i> 2013-2015
-                                            </h3>
-                                            <h4 class="timeline-title">
-                                                BA arts. Geography information system
-                                            </h4>
-                                            <p class="timeline-text">
-                                                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta laborum, quibusdam unde minus doloremque ad quod quos officiis nam nesciunt.
-                                            </p>
-                                         </div>                         
+                                                             
                                       </div>
                                   </div>
                               </div>
@@ -189,28 +177,67 @@
                                                <div class="timeline-item">
                                                   <div class="circle-dot"></div>
                                                   <h3 class="timeline-date">
-                                                      <i class="fa fa-calendar"></i> 2013-2015
+                                                      <i class="fa fa-calendar"></i> May 2021-Present 10 mos
                                                   </h3>
                                                   <h4 class="timeline-title">
-                                                      BA arts. Geography information system
+                                                      Zonal Representative
                                                   </h4>
                                                   <p class="timeline-text">
-                                                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta laborum, quibusdam unde minus doloremque ad quod quos officiis nam nesciunt.
+                                                     African Women in GIS (Egypt)
                                                   </p>
                                                </div>
                                                <!-- timeline item -->
                                                <div class="timeline-item">
                                                   <div class="circle-dot"></div>
                                                   <h3 class="timeline-date">
-                                                      <i class="fa fa-calendar"></i> 2013-2015
+                                                      <i class="fa fa-calendar"></i> Dec 2020-Jan 2022 1 yr 2 mos
                                                   </h3>
                                                   <h4 class="timeline-title">
-                                                      BA arts. Geography information system
+                                                      Business Development Specialist
                                                   </h4>
                                                   <p class="timeline-text">
-                                                      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Soluta laborum, quibusdam unde minus doloremque ad quod quos officiis nam nesciunt.
+                                                      SuperMap GIS
                                                   </p>
-                                               </div>                         
+                                               </div>    
+                                               <!-- timeline item -->
+                                               <div class="timeline-item">
+                                                <div class="circle-dot"></div>
+                                                <h3 class="timeline-date">
+                                                    <i class="fa fa-calendar"></i> Apr 2020-Apr 2021 1 yr 1 mos
+                                                </h3>
+                                                <h4 class="timeline-title">
+                                                    Technical Support Specialist
+                                                </h4>
+                                                <p class="timeline-text">
+                                                    EDGE Pro for Information System
+                                                </p>
+                                             </div>
+                                             <!-- timeline item -->
+                                             <div class="timeline-item">
+                                                <div class="circle-dot"></div>
+                                                <h3 class="timeline-date">
+                                                    <i class="fa fa-calendar"></i> Feb 2019-Apr 2020 1 yr 3 mos
+                                                </h3>
+                                                <h4 class="timeline-title">
+                                                    Technical Office
+                                                </h4>
+                                                <p class="timeline-text">
+                                                    Survey Tech
+                                                </p>
+                                             </div>
+                                             <!-- timeline item -->
+                                             <div class="timeline-item">
+                                                <div class="circle-dot"></div>
+                                                <h3 class="timeline-date">
+                                                    <i class="fa fa-calendar"></i> Mar 2019-Jan 2020 11 mos
+                                                </h3>
+                                                <h4 class="timeline-title">
+                                                    Technical Office
+                                                </h4>
+                                                <p class="timeline-text">
+                                                    Surveytech Avalon
+                                                </p>
+                                             </div>                     
                                             </div>
                                         </div>
                                     </div>
@@ -221,6 +248,139 @@
                 </div>
             </section>
             <!-- about section end -->
+            <!-- service section -->
+            <section class="service section" id="services">
+                <div class="container">
+                    <div class="row">
+                        <div class="section-title padd-15">
+                            <h2>Services</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- serviec item start -->
+                        <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-mobile-alt"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                        <!-- serviec item start -->
+                        <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-laptop-code"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                        <!-- serviec item start -->
+                        <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-code"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                         <!-- serviec item start -->
+                         <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-mobile-alt"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                        <!-- serviec item start -->
+                        <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-laptop-code"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                        <!-- serviec item start -->
+                        <div class="service-item">
+                            <div class="service-item-inner">
+                                <div class="icon">
+                                    <i class="fa fa-code"></i>
+                                </div>
+                                <h4>Web Design</h4>
+                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis architecto quos, odio deleniti explicabo optio a voluptate nihil nobis ex?</p>
+                            </div>
+                        </div>
+                        <!-- service item end -->
+                    </div>
+                </div>
+            </section>
+            <!-- service section end -->
+            <!-- portfolio section start -->
+            <section class="portfolio section" id="portfolio">
+                <div class="container">
+                    <div class="row">
+                        <div class="section-title padd-15">
+                            <h2>Portfolio</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="portfolio-heading">
+                            <h2>My Last Projects:</h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!-- portfolio item start -->
+                        <div class="portfolio-item padd-15">
+                            <div class="portfolio-item-inner shadow-dark">
+                                <div class="portfolio-img">
+                                    <img src="./images/gis-1.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- portfolio item end -->
+                          <!-- portfolio item start -->
+                          <div class="portfolio-item padd-15">
+                            <div class="portfolio-item-inner shadow-dark">
+                                <div class="portfolio-img">
+                                    <img src="./images/gis-2.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- portfolio item end -->
+                          <!-- portfolio item start -->
+                          <div class="portfolio-item padd-15">
+                            <div class="portfolio-item-inner shadow-dark">
+                                <div class="portfolio-img">
+                                    <img src="./images/gis-3.png" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- portfolio item end -->
+                          <!-- portfolio item start -->
+                          <div class="portfolio-item padd-15">
+                            <div class="portfolio-item-inner shadow-dark">
+                                <div class="portfolio-img">
+                                    <img src="./images/gis-4.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>
+                        <!-- portfolio item end -->
+                    </div>
+                </div>
+            </section>
+            <!-- portfolio section end -->
         </div>
         <!-- main content end -->
     </div>
